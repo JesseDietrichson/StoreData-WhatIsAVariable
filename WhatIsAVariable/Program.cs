c450dd26034c3ee4843a2290216dec2766499cd3
Jesse Dietrichson <jesse.dietrichson@xamarin.com>
2019-02-10 18:29:49 -0500

    Add project files.

---
+++ WhatIsAVariable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsAVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 5;
+            Console.WriteLine(myInt);
+
+            int myInt2;
+
+            myInt2 = 10;
+
+            Console.WriteLine(myInt2);
+
+            myInt2 = 15;
+
+            Console.WriteLine(myInt2);
+
+            //Basic data types
+
+            double myDouble = 5.5;
+            Console.WriteLine(myDouble);
+
+            string myString = "Hello World";
+            Console.WriteLine(myString);
+
+            char myChar = 'B';
+            Console.WriteLine(myChar);
+
+            bool myBool = true;
+            Console.WriteLine(myBool);
+        }
+    }
+}
